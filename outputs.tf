66ab0f6fae250ba1dd947661bd7d9e4cb4401d12
Siwoww <simone1ft93@gmail.com>
2025-06-17 14:37:29 +0200

    commit

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,14 @@
 #Se funziona segnare - count non funziona
-/*output "ssh-connect" {
+output "ssh-connect" {
     value = [
         for i in range(var.instance_number) :
         "ssh -i ${local_sensitive_file.private_key[idx].filename} ubuntu@${aws_instance.server[idx].public_ip}"
     ]
-}*/
+}
 
+/*
 output "ssh-connect" {
         value = "ssh -i ${local_sensitive_file.private_key[count.index].filename} ubuntu@${aws_instance.server[count.index].public_ip}"
 }
+#ERROR: The "count" object can only be used in "module", "resource", and "data" blocks, and only when the "count" argument is set.
+*/
