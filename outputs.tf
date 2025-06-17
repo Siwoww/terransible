1900fce200da745ea03615cd9ae453d98a7d2e17
Siwoww <simone1ft93@gmail.com>
2025-06-17 14:35:40 +0200

    commit

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,11 @@
 #Se funziona segnare - count non funziona
-output "ssh-connect" {
+/*output "ssh-connect" {
     value = [
         for i in range(var.instance_number) :
         "ssh -i ${local_sensitive_file.private_key[idx].filename} ubuntu@${aws_instance.server[idx].public_ip}"
     ]
+}*/
+
+output "ssh-connect" {
+        value = "ssh -i ${local_sensitive_file.private_key[count.index].filename} ubuntu@${aws_instance.server[count.index].public_ip}"
 }
