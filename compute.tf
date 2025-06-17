02bcc179b43fb8417994c0773c02622a87f0aea6
Siwoww <simone1ft93@gmail.com>
2025-06-17 12:34:21 +0200

    commit

--- compute.tf
+++ compute.tf
@@ -3,7 +3,7 @@ data "aws_ami" "ubuntu" {
     owners = ["amazon"]
 
   filter {
-    name = "Name"
+    name = "name"
     values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
   }
 }
