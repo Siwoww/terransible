bb4ec6531ce8dbfd1f4891b163d9c84a00219b18
Siwoww <simone1ft93@gmail.com>
2025-06-17 10:07:44 +0200

    commit

--- main.tf
+++ main.tf
@@ -44,6 +44,25 @@ resource "aws_default_route_table" "main_private_rt" {
   default_route_table_id = aws_vpc.main-vpc.default_route_table_id
 }
 
+resource "aws_subnet" "public_subnet" {
+  vpc_id = aws_vpc.main-vpc.id
+  cidr_block = var.public_subnet_cidr
+  map_customer_owned_ip_on_launch = true # associa IP pubblico alle istanze che ne fanno parte
+  availability_zone = data.aws_availability_zones.available.names[0]
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id = aws_vpc.main-vpc.id
+  cidr_block = var.private_subnet_cidr
+
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
 resource "random_id" "random" {
   byte_length = 2
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,1 +1,5 @@
 vpc_cidr = "10.11.0.0/16"
+
+public_subnet_cidr = "10.11.0.0/24"
+
+private_subnet_cidr = "10.11.1.0/24"
