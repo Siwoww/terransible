071a63001f07a3a41c7d436150197191584d3f92
Siwoww <simone1ft93@gmail.com>
2025-06-17 14:39:28 +0200

    commit

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,7 @@
 output "ssh-connect" {
     value = [
         for i in range(var.instance_number) :
-        "ssh -i ${local_sensitive_file.private_key[idx].filename} ubuntu@${aws_instance.server[idx].public_ip}"
+        "ssh -i ${local_sensitive_file.private_key[i].filename} ubuntu@${aws_instance.server[i].public_ip}"
     ]
 }
 
